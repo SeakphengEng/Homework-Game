3a9c70103618e6ab642ccce9cf35e0559bb517fb
Eng seakpheng <pheng0498@gmail.com>
2019-11-03 01:11:33 +0700

    Homework 4
    Please check

---
+++ Assets/Scripts/AtackerSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackerSpawner : MonoBehaviour
+{
+    
+   [SerializeField] float minSpawnerDelay = 1f;     
+   [SerializeField] float maxSpawnerDelay = 5f;
+   [SerializeField] Atacker attackerPrefab;
+    
+    bool spawn = true;
+    
+    IEnumerator Start()
+    {
+        while (spawn){
+            yield return new WaitForSeconds(Random.Range(minSpawnerDelay,maxSpawnerDelay));
+            SpawAttacker();
+        }
+       
+    }
+    private void SpawAttacker(){
+
+        Instantiate(attackerPrefab,transform.position,transform.rotation);
+    }
+    void Update(){
+
+    }
+}
